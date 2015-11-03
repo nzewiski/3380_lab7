2dc9da1e107b361e33f8269eb4cd14e721e8194f
nzewiski <monkeeeman3000@sbcglobal.net>
2015-11-02 22:50:35 -0600

    Fixed weird tab spacing

--- success.php
+++ success.php
@@ -1,17 +1,17 @@
 <html>
-	<head>
-		<!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
-		<title>Success</title>
-	</head>
-	<body>
-		<div class="container">
-			<h1>Success</h1>
-			<a href="index.php">
-				<button class="btn btn-info">Back to index</button>
-			</a>
-		</div>
-	</body>
+    <head>
+        <!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
+	<title>Success</title>
+    </head>
+    <body>
+	<div class="container">
+            <h1>Success</h1>
+            <a href="index.php">
+		<button class="btn btn-info">Back to index</button>
+            </a>
+	</div>
+    </body>
 </html>
